d73a16d5b7a73c33cbfdc1b128978f2b8f2e1c95
Felipe <spacetug@gmail.com>
2024-03-31 14:53:24 -0700

    (More) Cleanup the project

--- robot/src/main/kotlin/frc/robot/Robot.kt
+++ robot/src/main/kotlin/frc/robot/Robot.kt
@@ -7,7 +7,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj.xrp.XRPMotor
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.CommandScheduler
 import frc.subsystems.XRPDrivetrain
 
 class Robot : TimedRobot() {
@@ -52,7 +51,6 @@ class Robot : TimedRobot() {
     }
 
     override fun teleopExit() {
-        CommandScheduler.getInstance().cancelAll()
     }
 
     override fun testInit() {
